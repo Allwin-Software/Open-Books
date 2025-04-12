44bbcd1fcfd3cc0155b15d3a2e4bb3c1d504e412
Ravin Singh D <ravinsinghd@outlook.com>
2025-04-12 15:29:04 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoClientOptions } from "mongodb";
 
 const app = new Hono();
 let client = null;
@@ -8,11 +8,14 @@ app.get("/", async (c) => {
   console.log("request received");
   const dbConnectionString = (c.env as any).MONGODB_URI;
   console.log("Connection String", dbConnectionString);
-  client ??= new MongoClient((c.env as any).MONGODB_URI);
+  client ??= new MongoClient((c.env as any).MONGODB_URI, {
+    maxPoolSize: 1,
+    minPoolSize: 0,
+    serverSelectionTimeoutMS: 5000,
+  } as MongoClientOptions);
   const db = client.db("test");
   const coll = db.collection("test_workers");
 
-  await coll.drop().catch(() => null);
   await coll.insertOne({ a: new Date() });
 
   const result = await coll.findOne({});
