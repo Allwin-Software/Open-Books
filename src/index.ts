e5ec41835c058113def8402ce8d1778ef1fb286b
Ravin Singh D <ravinsinghd@outlook.com>
2025-04-12 15:16:33 +0530

    Added connection string

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const app = new Hono();
 let client = null;
 
 app.get("/", async (c) => {
+  console.log("request received");
+  const dbConnectionString = (c.env as any).MONGODB_URI;
+  console.log("Connection String", dbConnectionString);
   client ??= new MongoClient((c.env as any).MONGODB_URI);
   const db = client.db("test");
   const coll = db.collection("test_workers");
