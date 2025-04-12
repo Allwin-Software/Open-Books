2504df48a4f85bcee7461a9ed1cd3951c7252469
Ravin Singh D <ravinsinghd@outlook.com>
2025-04-12 14:57:51 +0530

    Added mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { MongoClient } from "mongodb";
 
-const app = new Hono()
+const app = new Hono();
+let client = null;
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  client ??= new MongoClient((c.env as any).MONGODB_URI);
+  const db = client.db("test");
+  const coll = db.collection("test_workers");
 
-export default app
+  await coll.drop().catch(() => null);
+  await coll.insertOne({ a: new Date() });
+
+  const result = await coll.findOne({});
+  return c.json(result);
+});
+
+export default app;
